48e63ed19cdedd6c4ac91c5f1572cdb52c112501
Julien Ramboz <ramboz@adobe.com>
2024-07-04 17:19:28 -0700

    fix: remove old api key

--- test-gdrive.js
+++ test-gdrive.js
@@ -3,7 +3,6 @@ import { getClient, CONTENT_TYPES } from './src/index.js';
 const client = await getClient({
   type: CONTENT_TYPES.GOOGLE_DRIVE,
   authConfig: {
-    // apiKey: 'AIzaSyBgR8wTwBV5UGuK_gCGVu6XxQnemHhtCxQ',
     keyFile: '/Users/ramboz/Projects/misc/helix-content-sdk/adobe-franklin-1654249406987-fa453067f4da.json',
   },
   documentStoreConfig: {
